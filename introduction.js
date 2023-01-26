4f0a12328bb486d30b9ba72cf715233247230ad3
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-26 19:19:35 +0600

    arithmetic operations add

--- introduction.js
+++ introduction.js
@@ -1,4 +1,10 @@
 console.log(9);//output show
+// variables rule    >>>>
+// variable always start with latter
+// variable can contain number with it
+// variable don't allow symbol except underscore
+// keywords  can't be variable.
+
 // number variables
 var price=5;
 var quantity=50;
@@ -12,3 +18,16 @@ console.log(address);
 // boolian variable
 var statu=true;
 console.log(statu);
+
+//arithmetic operations
+var first=10;
+var second=20;
+console.log(first+second);
+var result1=first+second;
+var result2=first-second;
+var result3=first*second;
+var result4=first/second;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
