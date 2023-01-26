a6539b48e908feb6ca2e57e1e35fa125776bd57a
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-26 19:12:35 +0600

    add variables rule

--- introduction.js
+++ introduction.js
@@ -1,4 +1,5 @@
 console.log(9);//output show
+// number variables
 var price=5;
 var quantity=50;
 var total=250;
@@ -8,3 +9,6 @@ var address="Dhaka";
 var area='Dhanmondi';
 console.log(area);
 console.log(address);
+// boolian variable
+var statu=true;
+console.log(statu);
