49ff7c7b80176b70de9f4a39f1f3043d2e89392c
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-26 19:02:39 +0600

    first commit

---
+++ introduction.js
@@ -0,0 +1,10 @@
+console.log(9);//output show
+var price=5;
+var quantity=50;
+var total=250;
+console.log(total);//output show
+// string in variable
+var address="Dhaka";
+var area='Dhanmondi';
+console.log(area);
+console.log(area);
