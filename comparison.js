b9bf4b0fd507f50508d950e7e2fce77c4effc97e
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-27 18:42:41 +0600

    use nested condition

--- comparison.js
+++ comparison.js
@@ -19,16 +19,34 @@ if(iphone<budget){
 else{
     console.log('i phone ami amneo kinum na');
 }
-var graduate=true;
+var graduate=false;
 var salary=50000;
-var car=false;
+var car=true;
 
-if(graduate==true && salary>=50000 && car==true){
-    console.log('cholo polai ');
+if(graduate==true || salary>=50000 && car==true){
+    console.log('eso prem kori na ');
 }
 else if(graduate===true && salary>=50000 || car==true){
     console.log('gari nai tao cholo polai jai');
 }
+else{
+    console.log('chakrita ami pabona bela sunso');
+}
+var pass=true;
+var math=80;
+var code=90;
+
+if(pass==true){
+    if(math>=80){
+        console.log('mathe A+ ');
+    }
+    else{console.log('jomlo na mathe');}
+    if(code>=80){
+        console.log('codea tumi sera ');
+    }
+    else{console.log('jomlo na codea');}
+}
+
 else{
     console.log('chakrita ami pabona bela sunso');
 }
