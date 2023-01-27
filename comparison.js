b0c35f1e8b10dab21ccac6b5e9a21bc4b5102f47
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-27 18:13:34 +0600

    touch conditionals

---
+++ comparison.js
@@ -0,0 +1,11 @@
+// console.log(5>6);
+// console.log(7>6);
+// console.log(6==6);
+// console.log(6!=6);
+// console.log(5<=6);
+// console.log(6<=6);
+console.log(7>=6);
+
+// money1 >money2 && result1 > result2
+// money1 >money2 || result1 > result2 
+// money1 >money2 || result1 > result2 && hight1==hight2
