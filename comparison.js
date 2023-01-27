5aa535a72438e581ade8872f38d6c6b3321d1258
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-27 18:20:17 +0600

    using if condition

--- comparison.js
+++ comparison.js
@@ -9,3 +9,21 @@ console.log(7>=6);
 // money1 >money2 && result1 > result2
 // money1 >money2 || result1 > result2 
 // money1 >money2 || result1 > result2 && hight1==hight2
+
+var iphone=79000;
+var budget=95000;
+
+if(iphone<budget){
+    console.log('i phone ami  kinum ');
+}
+else{
+    console.log('i phone ami amneo kinum na');
+}
+var graduate=true;
+
+if(graduate==true){
+    console.log('cholo polai ');
+}
+else{
+    console.log('chakrita ami pabona bela sunso');
+}
