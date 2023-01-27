62b8be78daa6e09b78bad91ae9655e3fcf87d599
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-27 18:05:55 +0600

    use push and pop

--- array.js
+++ array.js
@@ -16,3 +16,26 @@
     // index of
     console.log("index of :"+picnicfee.indexOf("free"));
     console.log("index of :"+picnicfee.indexOf("100"));
+    picnicfee.push(69,96);
+    console.log("push  last :"+ picnicfee);
+    // picnicfee.pop();
+    var v1=picnicfee.pop();
+    console.log("pop  :"+ picnicfee);
+    console.log("pop's item  :"+ v1);
+    // add in first
+    picnicfee.unshift(1000);
+    console.log("push 1st  :"+ picnicfee);
+    
+    // Add to a specified location
+    picnicfee.splice(2, 0, 500,200);
+    // 1st is where i add,second is how much dld after adding position 3rd is what i will add
+    console.log("push in  :"+ picnicfee);
+
+    // pop from first
+    picnicfee.shift();
+    console.log("pop first  :"+ picnicfee);
+
+    // pop by index
+    picnicfee.splice(1, 2); 
+    // Starting at index position 1, remove two elements 
+    console.log("pop first  :"+ picnicfee);
