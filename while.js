1e5a9011683a83395dc7f0808d7194712589b4d1
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-28 15:15:11 +0600

    print 0-10

--- while.js
+++ while.js
@@ -1,5 +1,13 @@
-var roastGiven=0;
-while(roastGiven<7){
-    console.log("roast den ");
-    roastGiven++;
+// ........................first touch
+// var roastGiven=0;
+// while(roastGiven<7){
+//     console.log("roast den ");
+//     roastGiven++;
+// }
+
+// ..............0-10print
+var num=0;
+while(num<=10){
+    console.log(num);
+    num++;
 }
