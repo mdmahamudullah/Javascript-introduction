ed9cb7c63e211abc40e4f17ad0fd507211f57f12
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-28 15:55:05 +0600

    try continue

--- array-loop.js
+++ array-loop.js
@@ -6,15 +6,28 @@
 
 // ..............break
 
-var number=[10,20,25,35,40,60,75,80,95,100,111];
-var search=112;
+// var number=[10,20,25,35,40,60,75,80,95,100,111];
+// var search=112;
+// var len=number.length;
+// for(var i=0;i<len;i++){
+//     if(number[i]==search){   
+//         console.log(number[i]+" found");
+//         break;
+//     }
+//     else if(i+1==len){
+//         console.log(" not found");
+//     }
+// }
+
+// ..............continue
+
+var number=[10,20,55,35,44,60,25,80,25,100,111];
 var len=number.length;
 for(var i=0;i<len;i++){
-    if(number[i]==search){   
-        console.log(number[i]+" found");
-        break;
+    if(number[i]<=40){   
+        continue;
     }
-    else if(i+1==len){
-        console.log(" not found");
+    else{
+        console.log(number[i]);
     }
 }
