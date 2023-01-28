81a23d44e77f00d88d7d5880cf427de4b24add3b
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-28 15:50:59 +0600

    use break

--- array-loop.js
+++ array-loop.js
@@ -1,5 +1,20 @@
+// var number=[10,20,25,35,40,60,75,80,95,100,111];
+// var len=number.length;
+// for(var i=0;i<len;i++){
+//     console.log(number[i]);
+// }
+
+// ..............break
+
 var number=[10,20,25,35,40,60,75,80,95,100,111];
+var search=112;
 var len=number.length;
 for(var i=0;i<len;i++){
-    console.log(number[i]);
+    if(number[i]==search){   
+        console.log(number[i]+" found");
+        break;
+    }
+    else if(i+1==len){
+        console.log(" not found");
+    }
 }
