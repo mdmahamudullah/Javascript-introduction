24b927ec06a4721e21f97a0838ba36d106f3f974
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-28 15:31:16 +0600

    print 0-10 for

--- for.js
+++ for.js
@@ -1,4 +1,12 @@
-var number=0;
-for(number=0;number<7;number++){
-    console.log("roast den");
+// ...............touch for loop
+
+// var number=0;
+// for(number=0;number<7;number++){
+//     console.log("roast den");
+// }
+
+// ...............print 0-10
+
+for(var i=0;i<=10;i++){
+    console.log(i);
 }
