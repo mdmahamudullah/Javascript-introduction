830bfc7b7951f1ab1c8c1a33ade1d9547a02e55a
mdmahamudullah <mahamudullah7k@gmail.com>
2023-01-28 16:18:13 +0600

    practice-1

---
+++ practise.js
@@ -0,0 +1,12 @@
+var fruits=['apple','banana','orange'];
+// find index of banana
+console.log("index of banana :"+fruits.indexOf("banana"));
+// replace banana by mango
+fruits.splice(1, 1, "mango");
+console.log(fruits);
+// remove orange
+fruits.pop();
+console.log(fruits);
+// add watermelon
+fruits.push("watermelon");
+console.log(fruits);
