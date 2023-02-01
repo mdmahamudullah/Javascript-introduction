c6067b8458df397da1671946c0404e61e25b4f82
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-01 16:24:09 +0600

    multiple parameter in function

--- function.js
+++ function.js
@@ -19,10 +19,22 @@
 
 //.................. simple function with parameter and call
 
-function singara(price){
-    console.log('function called successfully \n'+price);
-    console.log();
-}
+// function singara(price){
+//     console.log('function called successfully \n'+price);
+//     console.log();
+// }
 
-console.log('i call the function here ');
-singara(100);
+// console.log('i call the function here ');
+// singara(100);
+
+
+//.................. function with multiple parameter and call
+
+function add(num1,num2){
+
+    var sum=num1+num2;
+    console.log(sum);
+}
+var n1=10;
+var n2=20;
+add(n1,n2);
