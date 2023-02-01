e584907a6c81ac821e57eb33c3400ee578ae8610
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-01 16:18:26 +0600

    function with parameter

---
+++ function.js
@@ -0,0 +1,28 @@
+//............... simple function and call
+
+// function hello(){
+//     console.log('function called successfully');
+// }
+
+// console.log('i call the function here');
+// hello();
+
+
+//................. simple function and call
+
+// function hello(){
+//     console.log('function called successfully');
+// }
+
+// console.log('i call the function here '+hello());
+
+
+//.................. simple function with parameter and call
+
+function singara(price){
+    console.log('function called successfully \n'+price);
+    console.log();
+}
+
+console.log('i call the function here ');
+singara(100);
