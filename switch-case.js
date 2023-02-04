b4fdb5b017b55f0e80f108c1a6ad2de3ac89a3b8
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-04 17:56:11 +0600

    create switch case

---
+++ switch-case.js
@@ -0,0 +1,53 @@
+const color ="rd";
+
+if (color == "red") { 
+    console.log("The color is red");
+}
+else if (color == "orange") { 
+    console.log("The color is orange");
+}
+else if (color == "yellow") { 
+    console.log("The color is yellow");
+}
+else if (color == "green") { 
+    console.log("The color is green");
+}
+else if (color == "blue") { 
+    console.log("The color is blue");
+}
+else if (color == "indigo") { 
+    console.log("The color is indigo");
+}
+else if (color == "violet") { 
+    console.log("The color is violet");
+}
+else { 
+    console.log("That is not one of the seven colors.");
+}
+
+switch(color){
+    case "red":
+        console.log("The color is red");
+    break
+    case "orange":
+        console.log("The color is orange");
+    break
+    case "yellow":
+        console.log("The color is yellow");
+    break
+    case "green":
+        console.log("The color is green");
+    break
+    case "blue":
+        console.log("The color is blue");
+    break
+    case "indigo":
+        console.log("The color is indigo");
+    break
+    case "violet":
+        console.log("The color is violet");
+    break
+    default:
+        console.log("That is not one of the seven colors.");
+ 
+}
