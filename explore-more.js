2c3882e3237149f47cdd8451bf2575f152711c96
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-06 19:43:10 +0600

    join

--- explore-more.js
+++ explore-more.js
@@ -2,4 +2,7 @@ const myAddress="kulpaddi , Madaripur, Dhaka"
 
 console.log("string length: "+ myAddress.length);
 console.log("string length: "+ myAddress[3]);
+// join
+const elements = ['Fire', 'Air', 'Water'];
 
+console.log(elements.join(':'));
